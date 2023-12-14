c8913bbb8f30237b821c5035f76d27369406f536
michal <m.lejwoda@sunshineprofits.com>
2023-12-14 13:49:22 +0100

    LoginForm

--- trainer_page/src/components/auth/LoginForm.jsx
+++ trainer_page/src/components/auth/LoginForm.jsx
@@ -53,34 +53,50 @@ const LoginForm = () => {
 
     });
     return (
-        <form onSubmit={handleSubmit}>
-            <label htmlFor="email">Email Address</label>
-            <input
-                onChange={handleChange}
-                value={values.email}
-                id="email"
-                name="email"
-                type="email"
-                required
-                className={errors.email ? "input-error" : ""}
-            />
-            {errors.email && <p>{errors.email}</p>}
-            <label htmlFor="password">Hasło</label>
-            <input
-                value={values.password}
-                onChange={handleChange}
-                id="password"
-                name="password"
-                type="password"
-                autoComplete="current-password"
-                required
-                className={errors.password ? "input-error" : ""}
-                // className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-            />
-            {errors.password && <p>{errors.password}</p>}
-            {errorlogin && <p>{errorlogin}</p>}
-            <button type="submit">Zaloguj się</button>
-        </form>
+        <div className="booking__login bg-container-grey p-10 rounded-2xl">
+            <p className="text-4xl mb-4">Logowanie</p>
+            <div>
+                <form onSubmit={handleSubmit} className="w-full mt-5 ">
+                    <div className="flex justify-between mb-4">
+                        <div className="w-1/2 flex flex-col mr-3 ">
+                            <label className="mb-4 text-lg">Adres E-mail</label>
+                            <input
+                                onChange={handleChange}
+                                value={values.email}
+                                id="email"
+                                name="email"
+                                type="email"
+                                required
+                                placeholder="Adres email"
+                                className={errors.email ? "text-lg mr-3 py-2 px-3 rounded-lg input-error border-2 rounded-lg border-white" : "py-2 px-3 rounded-lg text-lg mr-3 border-2 rounded-lg border-white"}
+                            />
+                            {errors.email && <p>{errors.email}</p>}
+                        </div>
+                        <div className="w-1/2 flex flex-col ml-3">
+                            <label className="mb-4 text-lg">Hasło</label>
+                            <input
+                                value={values.password}
+                                onChange={handleChange}
+                                id="password"
+                                name="password"
+                                type="password"
+                                autoComplete="current-password"
+                                required
+                                placeholder="Hasło"
+                                className={errors.password ? "text-lg ml-3 py-2 px-3 rounded-lg input-error border-2 rounded-lg border-white" : "py-2 px-3 rounded-lg text-lg border-2 rounded-lg border-white"}
+                                // className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            />
+                        </div>
+                        {errors.password && <p>{errors.password}</p>}
+                        {errorlogin && <p>{errorlogin}</p>}
+                    </div>
+                    <div className="booking__login__button w-full flex justify-end">
+                        <button className="border-solid border-1 rounded-lg border-white mr-4" type="submit">Zaloguj się
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
 
     );
 
