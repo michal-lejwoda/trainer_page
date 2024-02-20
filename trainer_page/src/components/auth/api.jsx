d572040f6980da4e1f0d3a62351dd12806613892
michal <m.lejwoda@sunshineprofits.com>
2024-02-20 22:40:10 +0100

    fix

--- trainer_page/src/components/auth/api.jsx
+++ trainer_page/src/components/auth/api.jsx
@@ -1,11 +1,11 @@
 import axios from 'axios'
 
 const instance = axios.create();
-const DOMAIN = import.meta.env.VITE_DOMAIN
+// const DOMAIN = import.meta.env.VITE_DOMAIN
 
 
 export async function checkIfUserLogged() {
-    const response = await instance.get(`${DOMAIN}/api/users/me/`, {
+    const response = await instance.get(`/api/users/me/`, {
         withCredentials: true
     });
     return response.data
@@ -13,33 +13,33 @@ export async function checkIfUserLogged() {
 
 
 export async function getLogin(form) {
-    const response = await instance.post(`${DOMAIN}/api/token`, form);
+    const response = await instance.post(`/api/token`, form);
     return response.data
 
 
 }
 
 export async function postRegistration(form) {
-    const response = await instance.post(`${DOMAIN}/api/register_user`, form);
+    const response = await instance.post(`/api/register_user`, form);
     return response.data
 }
 
 export async function postSendResetPassword(form){
-    const response = await instance.post(`${DOMAIN}/api/send_reset_password_on_email`, form);
+    const response = await instance.post(`/api/send_reset_password_on_email`, form);
     return response
 }
 
 export async function postResetPassword(form){
-    const response = await instance.post(`${DOMAIN}/api/reset_password`, form);
+    const response = await instance.post(`/api/reset_password`, form);
     return response
 }
 
 export async function postGetUser(form){
-    const response = await instance.post(`${DOMAIN}/api/get_user`, form);
+    const response = await instance.post(`/api/get_user`, form);
     return response.data
 }
 
 export async function postMessageFromUser(form){
-    const response = await instance.post(`${DOMAIN}/api/send_direct_message`, form);
+    const response = await instance.post(`/api/send_direct_message`, form);
     return response
 }
