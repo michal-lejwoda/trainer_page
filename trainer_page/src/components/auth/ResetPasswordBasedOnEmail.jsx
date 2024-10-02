85cd59d50d9cd48fb9d22e0314bd4c8a10c1d899
michal <michal.lejwoda@gmail.com>
2024-10-02 17:23:02 +0200

    Reset Password Based on Email Done

--- trainer_page/src/components/auth/ResetPasswordBasedOnEmail.jsx
+++ trainer_page/src/components/auth/ResetPasswordBasedOnEmail.jsx
@@ -1,14 +1,13 @@
 import React, {useState} from 'react';
 
 import {useFormik} from 'formik';
-import {validateLogin, validateResetPasswordBasedOnEmail} from "./validation.jsx";
-import {checkIfUserLogged, getLogin, postSendResetPassword} from "./api.jsx";
-import {useCookies} from "react-cookie";
-import {useNavigate} from "react-router-dom";
-import {useAuth} from "./AuthContext.jsx";
+import {validateResetPasswordBasedOnEmail} from "./validation.jsx";
+import {postSendResetPassword} from "./api.jsx";
 import ReCAPTCHA from "react-google-recaptcha";
+import {useTranslation} from "react-i18next";
 
 const ResetPasswordBasedonEmail = () => {
+    const {t} = useTranslation()
     const CAPTCHA_SITE_KEY = import.meta.env.VITE_CAPTCHA_SITE_KEY
     const [resetmessage, setResetMessage] = useState('')
     const {values, setFieldValue, handleSubmit, handleChange, errors} = useFormik({
@@ -31,18 +30,18 @@ const ResetPasswordBasedonEmail = () => {
         let form = new FormData()
         form.append("email", values.email)
         await postSendResetPassword(form)
-        setResetMessage('Jeśli email istnieje w naszej bazie danych to na ten email została wysłana wiadomość wraz z linkiem umożliwiającym zmianę hasła')
+        setResetMessage(t("If the email exists in our database, an email has been sent with a link to reset your password."))
     }
 
     return (
         <div className="booking__login bg-container-grey p-10 rounded-2xl text-white">
             <div className="w-full md:w-1/2 ">
-                <p className="text-4xl mb-4">Przypomnienie hasła</p>
+                <p className="text-4xl mb-4">{t("Remind password")}</p>
                 <div>
                     <form onSubmit={handleSubmit} className="w-full mt-5 ">
                         <div className="flex justify-between mb-4">
                             <div className="w-full flex flex-col mr-3 ">
-                                <label className="mb-4 text-lg">Adres E-mail</label>
+                                <label className="mb-4 text-lg">{t("Email address")}</label>
                                 <input
                                     onChange={handleChange}
                                     value={values.email}
@@ -50,7 +49,7 @@ const ResetPasswordBasedonEmail = () => {
                                     name="email"
                                     type="email"
                                     required
-                                    placeholder="Adres email"
+                                    placeholder={t("Email address")}
                                     className={errors.email ? "bg-background-black-color text-lg mr-3 py-2 px-3 rounded-lg input-error border-2 rounded-lg border-white" : "bg-background-black-color py-2 px-3 rounded-lg text-lg mr-3 border-2 rounded-lg border-white"}
                                 />
                                 {errors.email && <p>{errors.email}</p>}
@@ -65,7 +64,8 @@ const ResetPasswordBasedonEmail = () => {
                             {errors.captcha && <p className="mt-3 text-red-800">{errors.captcha}</p>}
                         </div>
                         <div className="booking__login__button w-full flex flex-col items-end">
-                            <button className="border-solid border-1 rounded-lg border-white mr-4" type="submit">Wyślij
+                            <button className="border-solid border-1 rounded-lg border-white mr-4"
+                                    type="submit">{t("Send")}
                             </button>
                         </div>
                     </form>
