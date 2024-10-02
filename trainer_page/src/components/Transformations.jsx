e89019f06a8c7b5c40eca1f35eeebdf64702b385
michal <michal.lejwoda@gmail.com>
2024-10-02 15:05:20 +0200

    Transformations done

--- trainer_page/src/components/Transformations.jsx
+++ trainer_page/src/components/Transformations.jsx
@@ -2,8 +2,10 @@ import Aos from "aos";
 import {useEffect} from "react";
 import "aos/dist/aos.css";
 import {transformations} from "./transformations_store.jsx";
+import {useTranslation} from "react-i18next";
 
 function Transformations() {
+    const {t} = useTranslation()
     useEffect(() => {
         Aos.init({duration: 2000})
     }, []);
@@ -12,18 +14,15 @@ function Transformations() {
             <div className="transformations px-4 text-white overflow-x-hidden">
                 <div className="transformations--title my-8 text-center">
                     <strong>
-                        <span className="text-4xl text-center text-lighter-grey my-5">TRANSFORMACJE</span>
+                        <span
+                            className="text-4xl text-center text-lighter-grey my-5">{t("Clients Reverse Transformations")}</span>
                         <br/>
-                        <span className="text-4xl text-center mb-5">PODOPIECZNYCH</span>
+                        <span className="text-4xl text-center mb-5">{t("Transformations Reverse Clients")}</span>
                     </strong>
                 </div>
                 <div className="transformations--description">
-                    <p className="mb-7 text-base text-center">Przeprowadzę Cię przez cały proces od momentu, w którym
-                        jesteś teraz
-                        do uzyskania efektów ze zdjęć poniżej. Zyskasz zdrowie, lepszy nastrój i witalność, a nowe,
-                        zdrowe nawyki przekażesz swoim dzieciom. Podejmując współpracę ze mną poprawisz nie tylko swoją
-                        jakość życia, ale i całej rodziny. </p>
-                    <p className=" text-center font-semibold text-center">Pomagam przejść drogę po lepszą formę od 2012
+                    <p className="mb-7 text-base text-center">{t("I will guide you through the entire process from where you are now to achieving the results shown in the pictures below. You will gain health, a better mood, and vitality, and you will pass on new, healthy habits to your children. By working with me, you will improve not only your quality of life but that of your entire family.")}</p>
+                    <p className=" text-center font-semibold text-center">{t("I have been helping people on their journey to better fitness since 2012.")}
                         roku</p>
                 </div>
                 <div
--- trainer_page/src/components/transformations_store.jsx
+++ trainer_page/src/components/transformations_store.jsx
@@ -1,87 +1,104 @@
-export const transformations= [
+import {t} from "i18next";
+
+export const transformations = [
     {
         "name": "Ania",
-        "description": "Z Anią współpraca online trwała niespełna 5 miesięcy. Celem było spalenie zbędnego tłuszczu oraz uzyskanie atletycznej sylwetki.",
-        "realization": "Realizacja 100%",
+        "description": t("The online collaboration with Ania lasted just under 5 months. The goal was to burn excess" +
+                " fat and achieve an athletic physique."),
+        "realization": "Realization 100%",
         "image": "/Ewa_gnaslogo.png",
         "fade": "fade-right"
     },
     {
         "name": "Andrzej",
-        "description": "Z Andrzejem współpraca online trwała niespełna 5 miesięcy. Celem było spalenie zbędnego tłuszczu oraz uzyskanie atletycznej sylwetki.",
-        "realization": "Realizacja 100%",
+        "description": t("The online collaboration with Andrzej lasted just under 5 months. The goal was to burn" +
+                " excess fat and achieve an athletic physique."),
+        "realization": "Realization 100%",
         "image": "/Tomasz2.jpg",
         "fade": "fade-right"
     },
     {
         "name": "Tomek",
-        "description": "Z Tomkiem współpraca online trwała niespełna 5 miesięcy. Celem było spalenie zbędnego tłuszczu oraz uzyskanie atletycznej sylwetki.",
-        "realization": "Realizacja 100%",
+        "description": t("The online collaboration with Tomek lasted just under 5 months. The goal was to burn" +
+                    " excess" +
+                " fat and achieve an athletic physique."),
+        "realization": "Realization 100%",
         "image": "/Bartosz.jpg",
         "fade": "fade-left"
     },
     {
         "name": "Natalia",
-        "description": "Z Natalią współpraca online trwała niespełna 5 miesięcy. Celem było spalenie zbędnego tłuszczu oraz uzyskanie atletycznej sylwetki.",
-        "realization": "Realizacja 100%",
+        "description": t("The online collaboration with Natalia lasted just under 5 months. The goal was to burn" +
+                " excess fat and achieve an athletic physique."),
+        "realization": "Realization 100%",
         "image": "/Natalia.jpg",
         "fade": "fade-left"
     },
     {
         "name": "Piotr",
-        "description": "Z Piotrem współpraca online trwała niespełna 5 miesięcy. Celem było spalenie zbędnego tłuszczu oraz uzyskanie atletycznej sylwetki.",
-        "realization": "Realizacja 100%",
+        "description": t("The online collaboration with Piotr lasted just under 5 months. The goal was to burn" +
+                    " excess" +
+                " fat and achieve an athletic physique."),
+        "realization": "Realization 100%",
         "image": "/Piotr.jpg",
         "fade": "fade-right"
     },
     {
         "name": "Kazimierz",
-        "description": "Z Kazimierzem współpraca online trwała niespełna 5 miesięcy. Celem było spalenie zbędnego tłuszczu oraz uzyskanie atletycznej sylwetki.",
-        "realization": "Realizacja 100%",
+        "description": t("The online collaboration with Kazimierz lasted just under 5 months. The goal was to burn" +
+                " excess fat and achieve an athletic physique."),
+        "realization": "Realization 100%",
         "image": "/Kazimierz.png",
         "fade": "fade-right"
     },
     {
         "name": "Julia",
-        "description": "Z Julią współpraca online trwała niespełna 5 miesięcy. Celem było spalenie zbędnego tłuszczu oraz uzyskanie atletycznej sylwetki.",
-        "realization": "Realizacja 100%",
+        "description": t("The online collaboration with Julia lasted just under 5 months. The goal was to burn" +
+                    " excess" +
+                " fat and achieve an athletic physique."),
+        "realization": "Realization 100%",
         "image": "/Julia.png",
         "fade": "fade-left"
     },
     {
         "name": "Konrad",
-        "description": "Z Konradem współpraca online trwała niespełna 5 miesięcy. Celem było spalenie zbędnego tłuszczu oraz uzyskanie atletycznej sylwetki.",
-        "realization": "Realizacja 100%",
+        "description": t("The online collaboration with Konrad lasted just under 5 months. The goal was to burn" +
+                " excess fat and achieve an athletic physique."),
+        "realization": "Realization 100%",
         "image": "/Konrad.png",
         "fade": "fade-left"
     },
     {
         "name": "Oskar",
-        "description": "Z Oskarem współpraca online trwała niespełna 5 miesięcy. Celem było spalenie zbędnego tłuszczu oraz uzyskanie atletycznej sylwetki.",
-        "realization": "Realizacja 100%",
+        "description": t("The online collaboration with Oskar lasted just under 5 months. The goal was to burn" +
+                    " excess" +
+                " fat and achieve an athletic physique."),
+        "realization": "Realization 100%",
         "image": "/Oskar.jpg",
         "fade": "fade-right"
     },
     {
         "name": "Tomasz",
-        "description": "Z Tomaszem współpraca online trwała niespełna 5 miesięcy. Celem było spalenie zbędnego tłuszczu oraz uzyskanie atletycznej sylwetki.",
-        "realization": "Realizacja 100%",
+        "description": t("The online collaboration with Tomasz lasted just under 5 months. The goal was to burn" +
+                " excess fat and achieve an athletic physique."),
+        "realization": "Realization 100%",
         "image": "/Tomasz.jpg",
         "fade": "fade-right"
     },
     {
         "name": "Maja",
-        "description": "Z Mają współpraca online trwała niespełna 5 miesięcy. Celem było spalenie zbędnego tłuszczu oraz uzyskanie atletycznej sylwetki.",
-        "realization": "Realizacja 100%",
+        "description": t("The online collaboration with Maja lasted just under 5 months. The goal was to burn excess" +
+                " fat and achieve an athletic physique."),
+        "realization": "Realization 100%",
         "image": "/Maja.png",
         "fade": "fade-left"
     },
     {
         "name": "Mateusz",
-        "description": "Z Mateuszem współpraca online trwała niespełna 5 miesięcy. Celem było spalenie zbędnego tłuszczu oraz uzyskanie atletycznej sylwetki.",
-        "realization": "Realizacja 100%",
+        "description": t("The online collaboration with Mateusz lasted just under 5 months. The goal was to burn" +
+                " excess fat and achieve an athletic physique."),
+        "realization": "Realization 100%",
         "image": "/Mateusz.jpg",
         "fade": "fade-left"
     },
-
 ]
