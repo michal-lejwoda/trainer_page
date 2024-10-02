4a6d68127e36bf3841fbd313a4e52b1cd6c52fbd
michal <michal.lejwoda@gmail.com>
2024-10-02 11:45:11 +0200

    Finish about me in Homepage

--- trainer_page/src/components/Homepage.jsx
+++ trainer_page/src/components/Homepage.jsx
@@ -4,8 +4,10 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 import {useEffect} from "react";
 import Aos from "aos";
 import {NavLink} from "react-router-dom";
+import {useTranslation} from "react-i18next";
 
 function Homepage() {
+    const {t} = useTranslation()
     useEffect(() => {
         Aos.init({duration: 2000})
     }, []);
@@ -23,20 +25,16 @@ function Homepage() {
                 <div className="px-4">
                     <div>
                         <div className="mb-5 font-mono font-semibold text-center px-4">
-                            <p className="text-5xl text-lighter-grey my-2">Michał</p>
-                            <p className="text-6xl text-white mb-2">Trener</p>
-                            <p className="text-5xl text-lighter-grey my-2">Personalny</p>
+                            <p className="text-5xl text-lighter-grey my-2">{t("Michał")}</p>
+                            <p className="text-6xl text-white mb-2">{t("Personal")}</p>
+                            <p className="text-5xl text-lighter-grey my-2">{t("Trainer")}</p>
                         </div>
-                        <p className="text-lg px-4 text-center text-white">Pomogę Ci osiągnąć Twój zamierzony cel, a
-                            jeśli takiego
-                            jeszcze
-                            nie masz to
-                            obierzemy go razem i dojdziemy do końcowego rezultatu!</p>
+                        <p className="text-lg px-4 text-center text-white">{t("I will help you achieve your intended goal, and if you don't have one yet, we will set it together and reach the final result!")}</p>
 
                         <div className="flex flex-col items-center ">
                             <NavLink to="/reservation" className="text-white">
-                                <button className="mt-10  text-lg bg-button-grey text-white no-underline">Zarezerwuj
-                                    wizytę
+                                <button className="mt-10  text-lg bg-button-grey text-white no-underline">
+                                    {t("Book an appointment.")}
                                 </button>
                             </NavLink>
                         </div>
@@ -49,10 +47,10 @@ function Homepage() {
                         <div className="lg:w-1/2">
                             <div className="homepage__about_me--title my-8">
                                 <strong>
-                                    <span className="text-5xl text-lighter-grey my-5">PARĘ SŁÓW</span>
+                                    <span className="text-5xl text-lighter-grey my-5">{t("A FEW WORDS")}</span>
 
                                     <br/>
-                                    <span className="text-6xl mb-5 text-white">O MNIE</span>
+                                    <span className="text-6xl mb-5 text-white">{t("ABOUT ME")}</span>
                                 </strong>
                             </div>
                             <div className="shape-1 absolute z-[-1] right-0 top-20">
@@ -61,26 +59,10 @@ function Homepage() {
                             <div className="shape-2 absolute z-[-1] bottom-0 left-0">
                                 <img loading="lazy" src="/shape-2.png" alt="shape_2"/>
                             </div>
-                            <p className="my-5 text-white">Hej! Mam na imię Michał, jestem trenerem personalnym z
-                                certyfikatem EFA.
-                                Jestem absolwentem wychowania fizycznego na kierunku
-                                trener
-                                personalny w akademia wychowania fizycznego im. bronisława czecha w Krakowie.</p>
-                            <p className="mb-5 text-white">Zapraszam Cię do mnie na treningi jeśli masz problem ze
-                                zdrowiem,
-                                lub
-                                po prostu
-                                potrzebujesz pomocy w treningu siłowym, a przy okazji zadbać o swoją kondycję fizyczną i
-                                zdrowie.</p>
-                            <p className="font-semibold text-white">Pomogę Ci osiągnąć Twój zamierzony cel, a jeśli
-                                takiego jeszcze
-                                nie
-                                masz to
-                                obierzemy go razem i
-                                dojdziemy do końcowego rezultatu!</p>
-
+                            <p className="my-5 text-white">{t("Hi! My name is Michał, I am a personal trainer certified by the EFA. I graduated in physical education, specializing in personal training, from the Academy of Physical Education named after Bronisław Czech in Kraków.")}</p>
+                            <p className="mb-5 text-white">{t("I invite you to join my training sessions if you have health issues or simply need help with strength training, while also taking care of your physical fitness and health.")}</p>
+                            <p className="font-semibold text-white">{t("I will help you achieve your intended goal, and if you don't have one yet, we will set it together and reach the final result!")}</p>
                         </div>
-
                     </div>
                     <div data-aos="fade-right" className="homepage__personal_training relative my-8">
 
@@ -88,44 +70,23 @@ function Homepage() {
 
                             <div className="lg:w-1/2">
                                 <strong>
-                                    <span className="text-5xl text-lighter-grey my-5">Trening</span>
+                                    <span className="text-5xl text-lighter-grey my-5">{t("Personal Reverse Trainer")}</span>
                                     <br/>
-                                    <span className="text-6xl mb-5 text-white">Personalny</span>
+                                    <span className="text-6xl mb-5 text-white">{"Trainer Reverse Personal"}</span>
                                 </strong>
-                                <p className="text-2xl my-10 text-red-800 font-bold">Dlaczego mój trening będzie
-                                    odpowiedni
-                                    dla
-                                    ciebie?</p>
-                                <p className="text-white">Znajdziesz w nim mnóstwo przydatnych porad, wskazówek
-                                    dotyczących 4 kluczy do
-                                    sukcesu,
-                                    które
-                                    ci
-                                    pomogą: </p>
+                                <p className="text-2xl my-10 text-red-800 font-bold">{t("Why will my training be" +
+                                    " suitable for you?")}</p>
+                                <p className="text-white">{t("You will find plenty of useful tips and advice on the 4 keys to success that will help you:")} </p>
                                 <p className="my-5 text-left px-3 text-white"><font
-                                    className="text-red-700 text-xl">✓ </font>Odżywiaj
-                                    się
-                                    lepiej,
-                                    bez diety i bez poczucia niedostatku.</p>
+                                    className="text-red-700 text-xl">✓ </font>{t("Eat better, without dieting and without feeling deprived.")}</p>
                                 <p className="my-5 text-left px-3 text-white"><font
-                                    className="text-red-700 text-xl">✓ </font>Bądź
-                                    aktywny,
-                                    bez
-                                    względu na to, w jakiej jesteś obecnie formie.</p>
+                                    className="text-red-700 text-xl">✓ </font>{t("Be active, regardless of your current fitness level.")}</p>
                                 <p className="my-5 text-left px-3 text-white"><font
-                                    className="text-red-700 text-xl">✓ </font>Porzuć
-                                    zasady
-                                    żywieniowe, porzuć modne diety i sprzeczne porady.</p>
+                                    className="text-red-700 text-xl">✓ </font>{t("Ditch the food rules, ditch fad diets and conflicting advice.")}</p>
                                 <p className="my-5 text-left px-3 text-white"><font
-                                    className="text-red-700 text-xl">✓ </font>Wprowadź
-                                    fitness
-                                    do
-                                    swojego życia</p>
+                                    className="text-red-700 text-xl">✓ </font>{t("Incorporate fitness into your life.")}</p>
                                 <p className="my-5 text-left px-3 text-white"><font
-                                    className="text-red-700 text-xl">✓ </font>Osiągaj
-                                    i
-                                    utrzymuj
-                                    swoje cele, nawet gdy życie staje się napięte.</p>
+                                    className="text-red-700 text-xl">✓ </font>{t("Achieve and maintain your goals, even when life gets busy.")}</p>
                             </div>
                             <img
                                 className="inline-block object-cover  rounded-lg lg:w-1/3  lg:object-contain overflow-hidden "
