ddb936d03830e5278f688acad6dbf9eb3a15d5f5
michal <michal.lejwoda@gmail.com>
2024-10-09 20:48:56 +0200

    small fix

--- app/production.py
+++ app/production.py
@@ -1,6 +1,5 @@
 from .base import create_app
 
-# Lista dozwolonych originów dla środowiska produkcyjnego
 origins = ['https://www.trener-personalny-michal.pl', 'https://trener-personalny-michal.pl']
 
 app = create_app(origins)
