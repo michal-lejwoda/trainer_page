ae8e8822d465a5c31899090d623711e5b122eddb
michal <michal.lejwoda@gmail.com>
2024-10-14 10:13:55 +0200

    Add some translation

--- app/routers/validation.py
+++ app/routers/validation.py
@@ -11,7 +11,7 @@ from app.translation import trans as _
 def validate_user(user_id: int, db: Session) -> User:
     user = db.query(User).filter(User.id == user_id).first()
     if not user:
-        raise HTTPException(status_code=404, detail=_('User not found'))
+        raise HTTPException(status_code=404, detail=_("User not found"))
     return user
 
 def validate_work_hours(work_hours_id: int, db: Session) -> Type[WorkHours]:
@@ -27,5 +27,5 @@ def verify_user_permission(user: User, target_user: User):
 def get_work_hour_or_404(id: int, db: Session):
     element = db.query(WorkHours).filter(WorkHours.id == id).first()
     if element is None:
-        raise HTTPException(status_code=404, detail=_('Work hour not found'))
+        raise HTTPException(status_code=404, detail=_("Work hour not found"))
     return element
