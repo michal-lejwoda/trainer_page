156d97a5fb674c3b475a33b409a88cbf8004d3bf
michal <michal.lejwoda@gmail.com>
2024-10-18 12:39:59 +0200

    Some stripe css changes and language change

--- jobs/tasks.py
+++ jobs/tasks.py
@@ -6,12 +6,12 @@ from app.reservations.models import WorkHours
 def remove_unactive_records(db: Session):
     current_time = datetime.datetime.utcnow()
     current_time = current_time + datetime.timedelta(hours=1)
-    old_work_hours = db.query(WorkHours).filter(WorkHours.is_active == True, WorkHours.day < current_time.date()).all()
+    old_work_hours = db.query(WorkHours).filter(WorkHours.is_active == True, WorkHours.date < current_time.date()).all()
     for i in old_work_hours:
         i.is_active = False
         db.commit()
     today_work_hours = db.query(WorkHours).filter(WorkHours.is_active == True, WorkHours.day == current_time.date(),
-                                                  WorkHours.start_time < current_time.time()).all()
+                                                  WorkHours.start_datetime < current_time.time()).all()
     for i in today_work_hours:
         i.is_active = False
         db.commit()
--- trainer_page/src/components/CheckoutForm.jsx
+++ trainer_page/src/components/CheckoutForm.jsx
@@ -1,85 +1,83 @@
 import React, {useState} from 'react';
-import ReactDOM from 'react-dom';
 import {loadStripe} from '@stripe/stripe-js';
 import {
-  PaymentElement,
-  Elements,
-  useStripe,
-  useElements,
+    PaymentElement,
+    Elements,
+    useStripe,
+    useElements,
 } from '@stripe/react-stripe-js';
+import {useTranslation} from "react-i18next";
 
 const CheckoutForm = () => {
-  const stripe = useStripe();
-  const elements = useElements();
+    const stripe = useStripe();
+    const elements = useElements();
+    const [errorMessage, setErrorMessage] = useState(null);
 
-  const [errorMessage, setErrorMessage] = useState(null);
+    const handleSubmit = async (event) => {
+        event.preventDefault();
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
+        if (!elements) {
+            return;
+        }
 
-    if (elements == null) {
-      return;
-    }
+        const res = await fetch('/create-intent', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({amount: 1099, currency: 'pln', payment_method_types: ['card', 'blik', 'p24']}),
+        });
 
-    // Trigger form validation and wallet collection
-    const {error: submitError} = await elements.submit();
-    if (submitError) {
-      // Show error to your customer
-      setErrorMessage(submitError.message);
-      return;
-    }
+        const {client_secret: clientSecret} = await res.json();
 
-    // Create the PaymentIntent and obtain clientSecret from your server endpoint
-    const res = await fetch('/create-intent', {
-      method: 'POST',
-    });
+        const {error} = await stripe.confirmPayment({
+            elements,
+            clientSecret,
+            confirmParams: {
+                return_url: 'https://example.com/order/123/complete',
+            },
+        });
 
-    const {client_secret: clientSecret} = await res.json();
+        if (error) {
+            setErrorMessage(error.message);
+        } else {
+        }
+    };
 
-    const {error} = await stripe.confirmPayment({
-      //`Elements` instance that was used to create the Payment Element
-      elements,
-      clientSecret,
-      confirmParams: {
-        return_url: 'https://example.com/order/123/complete',
-      },
-    });
-
-    if (error) {
-      // This point will only be reached if there is an immediate error when
-      // confirming the payment. Show error to your customer (for example, payment
-      // details incomplete)
-      setErrorMessage(error.message);
-    } else {
-      // Your customer will be redirected to your `return_url`. For some payment
-      // methods like iDEAL, your customer will be redirected to an intermediate
-      // site first to authorize the payment, then redirected to the `return_url`.
-    }
-  };
-
-  return (
-    <form onSubmit={handleSubmit}>
-      <PaymentElement />
-      <button type="submit" disabled={!stripe || !elements}>
-        Pay
-      </button>
-      {/* Show error message to your customers */}
-      {errorMessage && <div>{errorMessage}</div>}
-    </form>
-  );
+    return (
+        <div className="flex w-100 justify-center">
+            <div className="w-3/4 m-5">
+                <form onSubmit={handleSubmit}>
+                    <PaymentElement/>
+                    <button type="submit" disabled={!stripe || !elements}>
+                        Pay
+                    </button>
+                    {errorMessage && <div>{errorMessage}</div>}
+                </form>
+            </div>
+        </div>
+    );
 };
 
 const stripePromise = loadStripe('pk_test_6pRNASCoBOKtIshFeQd4XMUh');
 
-const options = {
-  mode: 'payment',
-  amount: 1099,
-  currency: 'usd',
-  // Fully customizable with appearance API.
-  appearance: {
-    /*...*/
-  },
-};
 
-export default CheckoutForm
-// ReactDOM.render(<App />, document.body);
+const PaymentPage = () => {
+const {i18n} = useTranslation();
+const options = {
+    mode: 'payment',
+    amount: 1099,
+    locale: i18n.language,
+    currency: 'pln',
+    payment_method_types: ['card', 'p24'],
+    appearance: {
+        theme: 'night'
+    },
+};
+return (
+    <Elements stripe={stripePromise} options={options}>
+        <CheckoutForm/>
+    </Elements>
+
+)}
+;
+
+export default PaymentPage;
