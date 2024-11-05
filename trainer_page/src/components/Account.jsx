bf1beea907c2f260144de7f42d3daa632e8ec981
michal <michal.lejwoda@gmail.com>
2024-11-05 20:40:00 +0100

    fix redirection in Account

--- trainer_page/src/components/Account.jsx
+++ trainer_page/src/components/Account.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import AccountOrders from "./AccountOrders.jsx";
 import ResetPassword from "./auth/ResetPassword.jsx";
 import {useTranslation} from "react-i18next";
@@ -6,50 +6,59 @@ import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faLock, faWeight} from "@fortawesome/fontawesome-free-solid";
 import BoxLoading from "./BoxLoading.jsx";
 import SaveSettingsSuccessModal from "./modals/SaveSettingsSuccessModal.jsx";
+import {useAuth} from "./auth/AuthContext.jsx";
+import {useNavigate} from "react-router-dom";
 
-const Account = (props) => {
+const Account = () => {
     const {t} = useTranslation();
-    const [activeState, setActiveState] = useState('ACCOUNT_ORDERS')
-    const [successModal, setSuccessModal] = useState(false)
+    const [activeState, setActiveState] = useState('ACCOUNT_ORDERS');
+    const [successModal, setSuccessModal] = useState(false);
+    const navigate = useNavigate();
+    const {authUser} = useAuth();
+
+    useEffect(() => {
+        if (authUser == null) {
+
+            navigate("/login");
+        }
+    }, [authUser, navigate]);
+
+    if (authUser == null) {
+        return <BoxLoading />;
+    }
+
     return (
-        <>
-            {activeState ? (
-                <div className="settings">
-                    <h1 className="title settings__title">{t("Settings")}</h1>
-                    <div className="menu settings__menu">
-                        <div className="menu__elements">
-                            <div className="choices menu__choices">
-                                <div className="choices__element"
-                                     onClick={() => setActiveState("ACCOUNT_ORDERS")}>
-                                    <FontAwesomeIcon size="2x" icon={faWeight}/>
-                                </div>
-                                <div className="choices__element"
-                                     onClick={() => setActiveState("RESET_PASSWORD")}>
-                                    <FontAwesomeIcon size="2x" icon={faLock}/>
-                                </div>
-                            </div>
-                            <div className="menu__options">
-                                {activeState === 'ACCOUNT_ORDERS' &&
-                                    <AccountOrders setSuccessModal={setSuccessModal}/>
-                                }
-                                {activeState === 'RESET_PASSWORD' &&
-                                    <ResetPassword
-                                        // changePassword={props.changePassword}
-                                        // postLogoutAuth={props.postLogoutAuth}
-                                        setSuccessModal={setSuccessModal}
-                                    />
-                                }
-                            </div>
+        <div className="settings">
+            <h1 className="title settings__title">{t("Settings")}</h1>
+            <div className="menu settings__menu">
+                <div className="menu__elements">
+                    <div className="choices menu__choices">
+                        <div
+                            className={`choices__element ${activeState === 'ACCOUNT_ORDERS' ? 'active' : ''}`}
+                            onClick={() => setActiveState("ACCOUNT_ORDERS")}
+                        >
+                            <FontAwesomeIcon size="2x" icon={faWeight}/>
+                        </div>
+                        <div
+                            className={`choices__element ${activeState === 'RESET_PASSWORD' ? 'active' : ''}`}
+                            onClick={() => setActiveState("RESET_PASSWORD")}
+                        >
+                            <FontAwesomeIcon size="2x" icon={faLock}/>
                         </div>
                     </div>
-                    <SaveSettingsSuccessModal show={successModal} handleClose={() => setSuccessModal(false)}/>
+                    <div className="menu__options">
+                        {activeState === 'ACCOUNT_ORDERS' && (
+                            <AccountOrders setSuccessModal={setSuccessModal}/>
+                        )}
+                        {activeState === 'RESET_PASSWORD' && (
+                            <ResetPassword setSuccessModal={setSuccessModal}/>
+                        )}
+                    </div>
                 </div>
-            ) : (
-                <div className="box-loading">
-                    <BoxLoading/>
-                </div>
-            )}
-        </>
+            </div>
+            <SaveSettingsSuccessModal show={successModal} handleClose={() => setSuccessModal(false)}/>
+        </div>
     );
 };
+
 export default Account;
